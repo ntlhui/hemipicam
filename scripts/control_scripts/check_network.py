30c52001b9add31dd6ad88ed98231dc2cb0e25b7
Nathan Hui <ntlhui@gmail.com>
2018-09-07 19:25:16 -0700

    Updated to check IP address and updated output

--- scripts/control_scripts/check_network.py
+++ scripts/control_scripts/check_network.py
@@ -4,28 +4,38 @@
 # connected to the network.
 
 import socket
+import sys
 from multiprocessing.pool import ThreadPool
 
 cameraNumbers = range(1, 19)
 
 hostnames = ["hemipicam%02d.local" % (x) for x in cameraNumbers]
+# hostnames = ["192.168.1.%02d" % (x) for x in cameraNumbers]
 
 
 port = 22
 status = {}
 
-pool = ThreadPool()
+pool = ThreadPool(1)
 def checkHost(host):
 	global status
+	print(host)
 	s = socket.socket()
 	try:
 		s.connect((host, port))
 		s.close()
 		status[host] = True
 	except:
+		print("Unexpected error:", sys.exc_info()[0])
 		status[host] = False
 
 pool.map(checkHost, hostnames)
 
 for hostname in hostnames:
 	print("%s: %s" % (hostname, str(status[hostname])))
+
+alive = 0
+for hostname in hostnames:
+	if status[hostname]:
+		alive += 1
+print("%d of %d up" % (alive, len(hostnames)))
